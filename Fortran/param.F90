c24d1af5c73e6f43e7a7b1e53a00ab58631eb491
LydiaStefanova-NOAA <48454097+LydiaStefanova-NOAA@users.noreply.github.com>
2020-05-15 00:05:25 -0400

    Delete param.F90

--- Fortran/param.F90
+++
@@ -1,5 +0,0 @@
-  module param
-  implicit none
-  integer, parameter  :: nx = 384, ny = 384, nt=1, ntile=6,nkind=3, nexp=2 
-  integer, parameter  :: domask=1, varmin=4, varmax=134
-  end module param
