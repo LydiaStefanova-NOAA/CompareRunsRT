f28f7f1a7cb84a0b02e90466015c34e2a00aee2c
LydiaStefanova-NOAA <48454097+LydiaStefanova-NOAA@users.noreply.github.com>
2020-05-14 22:48:29 -0400

    Update runcompare.sh

--- runcompare.sh
+++ runcompare.sh
@@ -4,29 +4,25 @@
   module load netcdf/4.7.0
   CDF=/apps/netcdf/4.7.0/intel/18.0.5.274
 
-  nexp=2
+# How many experiments are to be compared with one another?
+  nexp=2  #number of experiments
 
+# What are the paths to each experiment?
   rootpath1="/scratch1/NCEPDEV/stmp2/Lydia.B.Stefanova/fromHPSS/forRegTest/IPD/"
   rootpath2="/scratch1/NCEPDEV/stmp2/Lydia.B.Stefanova/fromHPSS/forRegTest/CCPP/"
-  rootpath3="/scratch1/NCEPDEV/stmp2/Lydia.B.Stefanova/fromHPSS/forRegTest/CCPP/"
+  rootpath3="/scratch1/NCEPDEV/stmp2/Lydia.B.Stefanova/fromHPSS/forRegTest/Wave/"  #this directory  currently does not exist, just an example
+
+# Specify experiment names (for each of the above directories) - used for labeling output
   expname1="IPD"
   expname2="CCPP"
-  expname3="CCP2"
-
+  expname3="Wave" # this one currently does not exist
+  
+# Specify domain (atm/ocn/ice). File name ("file") phyf/ocn/ice) and extention ("extn") may have to be changed as needed. 
   domain="atm"; file="phyf"; extn="840.tile"
   #domain="ocn"; file="ocn"; extn="_2013_04_01_03.nc"
   #domain="ice"; file="ice"; extn="h_06h.2013-04-11-00000.nc"
 
-#
-  declare -a path=($rootpath1 $rootpaht2 $rootpath3)
-  declare -a expname=($expname1 $expname2 $expname3)
-  for (( i=0 ; i<$nexp; i+=1 )) ; do
-    echo $i
-    patharg="${patharg} ${path[$i]}$file$extn"
-    exparg="${exparg} ${expname[$i]}"
-  done
-  echo $patharg
-  echo $exparg
+
 
 case "$file" in
     "phyf") nx=384; ny=384; ntile=6; nkind=3; varmin=4; varmax=134; domask=1 ;;
@@ -39,7 +35,7 @@ esac
 cat << EOF > param.F90
   module param
   implicit none
-  integer, parameter  :: nx = $nx, ny = $ny, nt=1, ntile=$ntile,nkind=$nkind, nexp=$nexp 
+  integer, parameter  :: nx = $nx, ny = $ny, nt=1, ntile=$ntile, nkind=$nkind, nexp=$nexp 
   integer, parameter  :: domask=$domask, varmin=$varmin, varmax=$varmax
   end module param
 EOF
@@ -63,6 +59,18 @@ EOF
 
 make clean
 make
+
+# String up the file and experiment names to provide as arguments to the executable
+  declare -a path=($rootpath1 $rootpath2 $rootpath3)
+  declare -a expname=($expname1 $expname2 $expname3)
+  for (( i=0 ; i<$nexp; i+=1 )) ; do
+    echo $i
+    patharg="${patharg} ${path[$i]}$file$extn"
+    exparg="${exparg} ${expname[$i]}"
+  done
+  echo $patharg
+  echo $exparg
+  
  ./runcompare $patharg $exparg 
 
 
